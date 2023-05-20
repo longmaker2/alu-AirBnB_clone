519514f05b7d90d3c60bf470c4752b966c22bca0
longmaker2 <l.deng@alustudent.com>
2023-05-20 11:23:47 +0200

    updated tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for models/amenity.py.
-
-Unittest classes:
-    TestAmenity_instantiation
-    TestAmenity_save
-    TestAmenity_to_dict
+"""
+Module documentation
 """
 import os
 import models
@@ -184,6 +180,40 @@ class TestAmenity_to_dict(unittest.TestCase):
         am = Amenity()
         with self.assertRaises(TypeError):
             am.to_dict(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """ Test the Amenity class """
+
+    def test_instance(self):
+        """ Test instance """
+        obj = Amenity()
+        self.assertIsInstance(obj, Amenity)
+
+    def test_is_subclass(self):
+        """test the instance of sub classes"""
+        amenity = Amenity()
+        self.assertTrue(issubclass(type(amenity), BaseModel))
+
+    def test_name(self):
+        """test name"""
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+        amenity.name = "Wifi"
+        self.assertEqual(amenity.name, "Wifi")
+        self.assertIsNotNone(amenity.id)
 
 
 if __name__ == "__main__":
