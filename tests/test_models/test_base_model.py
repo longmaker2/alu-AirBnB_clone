36547d51c6687d2181d31c25ab17e2cdce1b8edf
longmaker2 <l.deng@alustudent.com>
2023-05-20 10:07:21 +0200

    updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
-
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
+"""
+    Defines unittests for 'models/base_model.py'
+    Unittest classes:
+        TestBaseModel_instantiation
+        TestBaseModel_save
+        TestBaseModel_to_dict
 """
 import os
 import models
@@ -15,21 +15,28 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the BaseModel class."""
+    ''' Unittests for testing instantiation of the BaseModel class. '''
 
     def test_no_args_instantiates(self):
+        # It checks if the class BaseModel is,
+        # the same type as the instance of BaseModel.
         self.assertEqual(BaseModel, type(BaseModel()))
 
     def test_new_instance_stored_in_objects(self):
+        # It checks if the BaseModel object is in the storage.all().values()
         self.assertIn(BaseModel(), models.storage.all().values())
 
     def test_id_is_public_str(self):
+        # It checks if the type of the id attribute,
+        # of the BaseModel class is a string.
         self.assertEqual(str, type(BaseModel().id))
 
     def test_created_at_is_public_datetime(self):
+        # It checks if the type of the created_at attribute is datetime.
         self.assertEqual(datetime, type(BaseModel().created_at))
 
     def test_updated_at_is_public_datetime(self):
+        # It checks if the type of the updated_at attribute is datetime.
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
     def test_two_models_unique_ids(self):
@@ -50,6 +57,13 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertLess(bm1.updated_at, bm2.updated_at)
 
     def test_str_representation(self):
+        # 1. We’re creating a datetime object.
+        # 2. We’re creating a BaseModel object.
+        # 3. We’re setting the id, created_at, and updated_at,
+        # attributes of the BaseModel object.
+        # 4. We’re calling the __str__ method of the BaseModel object.
+        # 5. We’re checking that the output of the __str__ method,
+        # contains the id, created_at, and updated_at attributes.
         dt = datetime.today()
         dt_repr = repr(dt)
         bm = BaseModel()
@@ -66,6 +80,13 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertNotIn(None, bm.__dict__.values())
 
     def test_instantiation_with_kwargs(self):
+        # 1. We create a new instance of the BaseModel class.
+        # 2. We set the id attribute to “345”.
+        # 3. We set the created_at attribute to the current time.
+        # 4. We set the updated_at attribute to the current time.
+        # 5. We check that the id attribute is set to “345”.
+        # 6. We check that the created_at attribute is set to the current time.
+        # 7. We check that the updated_at attribute is set to the current time.
         dt = datetime.today()
         dt_iso = dt.isoformat()
         bm = BaseModel(id="345", created_at=dt_iso, updated_at=dt_iso)
@@ -78,6 +99,15 @@ class TestBaseModel_instantiation(unittest.TestCase):
             BaseModel(id=None, created_at=None, updated_at=None)
 
     def test_instantiation_with_args_and_kwargs(self):
+        # 1. We create a datetime object with the current time.
+        # 2. We convert the datetime object to a string in ISO 8601 format.
+        # 3. We create a BaseModel object with the id “345” and,
+        # the created_at and updated_at attributes set to the ISO 8601 string.
+        # 4. We check that the id attribute is set to “345”.
+        # 5. We check that the created_at attribute is set to,
+        # the datetime object.
+        # 6. We check that the updated_at attribute is set to,
+        # the datetime object.
         dt = datetime.today()
         dt_iso = dt.isoformat()
         bm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
@@ -87,7 +117,9 @@ class TestBaseModel_instantiation(unittest.TestCase):
 
 
 class TestBaseModel_save(unittest.TestCase):
-    """Unittests for testing save method of the BaseModel class."""
+    """
+        Unittests to test save method of the 'BaseModel' class.
+    """
 
     @classmethod
     def setUp(self):
@@ -139,7 +171,9 @@ class TestBaseModel_save(unittest.TestCase):
 
 
 class TestBaseModel_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the BaseModel class."""
+    """
+        Unittests to test 'to_dict' method of the 'BaseModel' class.
+    """
 
     def test_to_dict_type(self):
         bm = BaseModel()
